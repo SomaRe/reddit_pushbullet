451a9c8a148cc1b2f01dcce4dd34d3ad8a823df7
shiva tej soma <shivatej82@gmail.com>
2023-09-22 16:07:14 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import praw
+import re
+import time
+from pushbullet import Pushbullet
+import keys
+
+
+class RedditScanner:
+    def __init__(self, subreddit, regex, parameters):
+        self.subreddit_name = subreddit
+        self.regex = regex
+        self.parameters = parameters
+        self.last_post_time = 0
+        self.last_post_time_available = False
+        self.reddit = praw.Reddit(
+            client_id=keys.reddit_client_id,
+            client_secret=keys.reddit_client_secret,
+            user_agent="bestdeals",
+        )
+        self.pb = Pushbullet(keys.PUSHBULLET_ACCESS_TOKEN)
+
+    def _matches_criteria(self, title, flair):
+        if flair not in ["Selling", "Selling\\Trading"]:
+            return False
+        if re.search(self.regex, title):
+            return True
+        if any(param in title for param in self.parameters):
+            return True
+        return False
+
+    def scan_new_posts(self):
+        if not self.last_post_time_available:
+            for submission in self.reddit.subreddit(self.subreddit_name).new(limit=None):
+                if self._matches_criteria(submission.title.lower(), submission.link_flair_text):
+                    self.last_post_time = submission.created_utc
+                    self.last_post_time_available = True
+                    self.pb.push_note(submission.title, submission.url)
+                    break
+        else:
+            posts_to_notify = []
+            for submission in self.reddit.subreddit(self.subreddit_name).new(limit=None):
+                if submission.created_utc <= self.last_post_time:
+                    break
+                if self._matches_criteria(submission.title.lower(), submission.link_flair_text):
+                    posts_to_notify.append(submission)
+
+            for post in reversed(posts_to_notify):
+                self.pb.push_note(post.title, post.url)
+            if posts_to_notify:
+                self.last_post_time = posts_to_notify[-1].created_utc
+
+
+def main():
+    scanner = RedditScanner(
+        subreddit="CanadianHardwareSwap",
+        regex=r"[\d]{4,5}",
+        parameters=["ryzen", "5600", "5600x"]
+    )
+
+    while True:
+        scanner.scan_new_posts()
+        time.sleep(300)
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Reddit Scanner with Pushbullet Notifications
+
+This project scans specific subreddits for new posts that match given criteria and sends notifications with the post title and link to your devices using Pushbullet.
+
+## Features
+
+- Searches for new posts in specified subreddits.
+- Filters posts based on specific criteria (Regex and parameters).
+- Sends notifications with post title and link via Pushbullet.
+
+## Setup
+
+### Prerequisites
+
+- Python 3.x
+- Pushbullet account and API key
+- Reddit `client_id`, `client_secret`, and `user_agent` for PRAW
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   git clone <repository_url>
+   ```
+
+2. Navigate to the project directory and install the required packages:
+   ```bash
+   cd reddit-scanner
+   pip install praw pushbullet.py
+   ```
+
+3. Set up your keys. Rename `keys_example.py` to `keys.py` and fill in the necessary API keys and Reddit credentials.
+
+## Usage
+
+Run the main script:
+```bash
+python main.py
+```
+
+The script will periodically scan the specified subreddit for new posts based on your criteria and send a Pushbullet notification for each match.
+
+## Configuration
+
+You can modify the following configurations in `main.py`:
+
+- `subreddit`: Name of the subreddit to scan.
+- `regex`: Regular expression to match against post titles.
+- `parameters`: List of keywords to check in post titles.
